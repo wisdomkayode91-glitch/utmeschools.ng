7282564e15f4954f6b0263acea9ba4e7aa464ec8
wisdomkayode91-glitch <wisdomkayode91@gmail.com>
2026-09-22 09:10:04 +0100

    Add files via upload

--- coach.js
+++ coach.js
@@ -332,5 +332,4 @@ function showToast(msg) {
   if (!t) return;
   t.textContent = msg; t.classList.add('show');
   clearTimeout(tt); tt = setTimeout(() => t.classList.remove('show'), 2500);
-       }
-                                   
+}
